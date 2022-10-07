04bd17f57b3102e652c9d83610bbb9f7f5f5c472
Yghore-2 <yhgore@gmail.com>
2022-10-07 14:11:28 +0200

    Test d'un système de récupèrer des json (classe avec deux méthode statique)

--- src/main/java/fr/yghore/Models/Data.java
+++ src/main/java/fr/yghore/Models/Data.java
@@ -1,9 +1,14 @@
 package fr.yghore.Models;
 
 import fr.yghore.dyglib.Data.Json;
+import fr.yghore.dyglib.Data.Salvageable;
+import fr.yghore.dyglib.Logger;
 
-import java.io.File;
 import java.io.FileNotFoundException;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Parameter;
+import java.lang.reflect.TypeVariable;
 import java.nio.file.Path;
 
 public class Data
@@ -27,6 +32,36 @@ public class Data
         new Json(User.class, Path.of(ConfigData.getConfig().getUsersFolder(), memberId + ".json").toString()).save(data);
 
     }
+
+    public static Salvageable getData(String path, Class<? extends Salvageable> cl,Object... paramConst) {
+        return getData(path, cl, 0, paramConst);
+    }
+
+    public static Salvageable getData(String path, Class<? extends Salvageable> cl, int constructor,Object... paramConst) {
+        try{
+            return new Json(cl, Path.of(path).toString()).load();
+
+        }
+        catch(FileNotFoundException e)
+        {
+            try
+            {
+                return (Salvageable) cl.getDeclaredConstructors()[constructor].newInstance(paramConst);
+
+            } catch (InvocationTargetException | InstantiationException | IllegalAccessException ex) {
+                Logger.getLogger().sendError("Une erreur est survenue lors de la création d'un nouveau fichier de donnée... (mauvais argument passé, etc...)");
+                ex.printStackTrace();
+            }
+        }
+        return new Salvageable();
+    }
+
+    public static void saveData(String path, Salvageable data)
+    {
+        new Json(User.class, Path.of(path).toString()).save(data);
+
+    }
+
 
 
 }
--- src/test/java/testMain.java
+++ src/test/java/testMain.java
@@ -1,16 +1,22 @@
-import fr.yghore.Utils.TimeFormat;
-
-import java.time.Duration;
-import java.time.temporal.ChronoUnit;
+import fr.yghore.Models.Data;
+import fr.yghore.Models.Joke;
+import fr.yghore.Models.User;
 
 public class testMain {
 
     public static void main(String[] args) {
-        Duration duration = Duration.parse("P160DT3H4M");
-        System.out.println(duration.toString());
 
-        Duration dur = TimeFormat.parse("60d30h20m10s");
-        System.out.println(dur);
+
+        Joke test = (Joke) Data.getData("joke.json", Joke.class);
+        test.addJoke("Je suis une blague");
+        test.addJoke("Je suis une autre blague");
+        test.addJoke("Comment va bien le monde ?");
+        Data.saveData("joke.json", test);
+
+        Joke joke = (Joke) Data.getData("joke.json", Joke.class);
+
+        System.out.println(joke.getJoke(2));
+        System.out.println(joke.getJoke(3));
 
 
     }
